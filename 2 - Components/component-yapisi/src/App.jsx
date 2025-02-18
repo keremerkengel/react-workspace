2ea57bf87ce00ff7403fddd9487a7550bc502aaf
Kerem Erkengel <keremerkengel@gmail.com>
2025-02-18 12:53:53 +0300

    Yeni proje eklendi: Register Modal Yapıldı

---
+++ 2 - Components/component-yapisi/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import RegisterModal from './RegisterModal'
+
+function App() {
+  
+
+  return (
+    <>
+    <RegisterModal />
+    </>
+  )
+}
+
+export default App
---
+++ 2 - Components/component-yapisi/src/RegisterModal.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+function RegisterModal() {
+  return (
+    <div>
+        <div style={{ backgroundColor: 'lightblue', padding: '5px', borderRadius: '10px', width: '600px', margin: 'auto', marginTop: '100px' }}>
+      <h1>Kayıt Ekranına Hoş Geldiniz</h1>
+      <p>Kullanıcı Adınız</p>
+      <input type="text" />
+      <p>Parolanız</p>
+      <input type="password" /><br />
+      <button>Kayıt Ol</button>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default RegisterModal
